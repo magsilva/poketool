f07523182d4b2943486a1fdb64e4c9391b2fdb15
rmartins <rmartins@f926b94f-7163-4062-b5b3-3dd880a2c0c2>
2012-07-06 22:13:14 +0000

    Removed license from some .c files which aren't source files
    git-svn-id: http://ccsl.icmc.usp.br/svn/poke-tool@15 f926b94f-7163-4062-b5b3-3dd880a2c0c2

--- li/pokelib7.c
+++ li/pokelib7.c
@@ -70,8 +70,7 @@ char * tabpath;
   raiz = NULL;
   while (fscanf(fp,"%s",linha) != EOF)
      { 
-       //int n_match = (int) NULL;
-       int *n_match = (int *)NULL;
+       int n_match = (int) NULL;
        raiz = tree_tipo(raiz,linha,1,n_match);
      }
   fclose(fp);
