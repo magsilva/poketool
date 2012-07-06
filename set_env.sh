655898da0432138a65aa7aaf93a87e92189a0932
rmartins <rmartins@f926b94f-7163-4062-b5b3-3dd880a2c0c2>
2012-07-06 21:45:07 +0000

    Added script to set environment variables
    git-svn-id: http://ccsl.icmc.usp.br/svn/poke-tool@13 f926b94f-7163-4062-b5b3-3dd880a2c0c2

---
+++ set_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Note: This script only works when run with 'source set_env.sh' or '. set_env.sh'."
+
+DIR=$(dirname $(readlink -f $1))
+export NEWPOKETABS="$DIR/bin/poketabs"
+export NEWLITABS="$DIR/bin/newlitabs"
+export PATH="$PATH:$DIR/bin"
